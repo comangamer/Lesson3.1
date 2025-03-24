000ca1fdaa32b3e80379ea12aa861c89f105dab8
gmiiv001 <gmiiv001@edu.xamk.fi>
2025-03-24 15:53:26 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        //isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            dirtParticle.Play();
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            
+            gameOver = true;
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            dirtParticle.Stop();
+            explosionParticle.Play();
+            playerAnim.SetInteger("DeathType_int", 1);
+            
+        }
+    }
+
+}
+
