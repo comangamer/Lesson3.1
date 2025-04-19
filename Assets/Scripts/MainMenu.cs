bbfa123ce246a34b0470e116c2ef0d697659513f
comangamer <comangamer@inbox.ru>
2025-04-19 23:31:47 +0300

    Added Main Menu (not working)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections; // Для работы с корутинами
+using System.Collections.Generic; // Для работы с коллекциями
+using UnityEngine;
+using UnityEngine.SceneManagement; // Для загрузки сцен
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject SettingsCanvas; // Ссылка на объект настроек
+    public GameObject AboutCanvas; // Ссылка на объект "О нас"
+    public GameObject InstructionBG; // Ссылка на объект с инструкцией
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
